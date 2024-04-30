cda0916a0eb85d2832a345e0ccf4994ada9d8f72
christianmonfiston <christianoliviermonfiston@gmail.com>
2024-04-30 10:45:45 -0400

    made changes

--- src/Components/Careers.jsx
+++ src/Components/Careers.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import "../Styles/Careers.scss";
+import '../Styles/Careers.scss';
 import { motion } from 'framer-motion';
 import { careers } from '../Images';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Careers = () => {
+  const handleViewPositions = () => {
+    toast.info('We currently have no jobs available.', {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   return (
     <div className="careers">
       <motion.div
@@ -14,9 +28,12 @@ const Careers = () => {
       >
         <h1 className="careers__title">Join Our Team</h1>
         <p className="careers__description">
-          At Lunexis, we are constantly seeking talented individuals to join our dynamic team. We value creativity, innovation, and a passion for digital entertainment. If you share our vision and thrive in a collaborative environment, we invite you to explore our current career opportunities.
+          At Lunexis, we are constantly seeking talented individuals to join our dynamic team. We
+          value creativity, innovation, and a passion for digital entertainment. If you share our
+          vision and thrive in a collaborative environment, we invite you to explore our current
+          career opportunities.
         </p>
-        <a href="#" className="careers__cta">
+        <a href="#" onClick={handleViewPositions} className="careers__cta">
           View Open Positions
         </a>
       </motion.div>
@@ -28,6 +45,7 @@ const Careers = () => {
       >
         <img src={careers} alt="Careers" />
       </motion.div>
+      <ToastContainer />
     </div>
   );
 };
